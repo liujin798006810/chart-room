7ca3b155b5b9379aa1d7deba1e843849d40deb8a
liujin <798006810@qq.com>
2019-10-21 01:27:24 +0800

    导入项目

---
+++ server/storage.js
@@ -0,0 +1,11 @@
+const STORAGE_KEY = 'chatlist';
+
+export default {
+    fetch: function () {
+        return JSON.parse(window.sessionStorage.getItem(STORAGE_KEY) || '[]');
+    },
+
+    save: function (item) {
+        window.sessionStorage.setItem(STORAGE_KEY, JSON.stringify(item));
+    }
+}
